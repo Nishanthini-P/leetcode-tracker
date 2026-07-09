a506a435d7e5fb4fb06ff6bdb61a2606456a8710
Nishanthini-P <24243068@drngpit.ac.in>
2026-07-09 10:07:18 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.py
@@ -0,0 +1,28 @@
+# Last updated: 7/9/2026, 10:07:19 AM
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev = 0
+        
+        # Traverse from right to left
+        for char in reversed(s):
+            value = roman[char]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+            prev = value
+        
+        return total
+
+        
