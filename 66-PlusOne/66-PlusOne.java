39d923afb93eafddf6c2401da088d3a60ba45ba6
Nishanthini-P <24243068@drngpit.ac.in>
2026-07-09 10:06:51 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.java
@@ -0,0 +1,24 @@
+// Last updated: 7/9/2026, 10:06:51 AM
+class Solution {
+    public int[] plusOne(int[] digits) {
+
+        // Traverse from last digit
+        for (int i = digits.length - 1; i >= 0; i--) {
+
+            // If digit is less than 9, increment and return
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+
+            // If digit is 9, make it 0 and carry over
+            digits[i] = 0;
+        }
+
+        // If all digits were 9 (e.g., 999 -> 1000)
+        int[] result = new int[digits.length + 1];
+        result[0] = 1;
+
+        return result;
+    }
+}
