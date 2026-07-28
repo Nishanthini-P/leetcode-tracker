1c28b5db71bec2736eef96e3431eaa6ecc71d8f7
Nishanthini-P <24243068@drngpit.ac.in>
2026-07-28 15:10:41 +0530

    Create 8-StringtoInteger(atoi)

---
+++ 8-StringtoInteger(atoi)/version/java/8-StringtoInteger(atoi)_20260728_151040.java
@@ -0,0 +1,17 @@
+// Last updated: 7/28/2026, 3:10:40 PM
+1class Solution {
+2    public int maxProfit(int[] prices) {
+3        int minPrice = Integer.MAX_VALUE;
+4        int maxProfit = 0;
+5
+6        for (int price : prices) {
+7            if (price < minPrice) {
+8                minPrice = price;
+9            } else if (price - minPrice > maxProfit) {
+10                maxProfit = price - minPrice;
+11            }
+12        }
+13
+14        return maxProfit;
+15    }
+16}
